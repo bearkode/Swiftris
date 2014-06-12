61ae3746b3a53479a3f2c7ae5c0697c76ff2dca8
Chang-Gi, Kim <cgkim@mac.com>
2014-06-12 20:45:19 +0900

    Initial Commit

---
+++ Swiftris/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swiftris
+//
+//  Created by cgkim on 2014. 6. 12..
+//  Copyright (c) 2014년 NHN Entertainment. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swiftris/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Swiftris
+//
+//  Created by cgkim on 2014. 6. 12..
+//  Copyright (c) 2014년 NHN Entertainment. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
