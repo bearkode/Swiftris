8db0cab3018015cfd6e0edca2087e08d394d1cd8
cgkim <changgi.kim@navercorp.com>
2016-07-29 20:11:40 +0900

    Oops

---
+++ Logic/State.swift
@@ -0,0 +1,22 @@
+/*
+ *  State.swift
+ *  Swiftris
+ *
+ *  Created by bearkode on 2016. 7. 29..
+ *  Copyright © 2016 bearkode. All rights reserved.
+ *
+ */
+
+import Foundation
+
+
+enum State {
+
+    case ready
+    case playing
+    case pause
+    case gameOver
+
+}
+
+
